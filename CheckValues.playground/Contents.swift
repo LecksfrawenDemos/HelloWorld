d2a9a11a6718e13be60df7197296cf82a1f7d392
Héctor De Diego <lecksfrawen@gmail.com>
2019-02-20 13:00:42 -0600

    Agregado playground
    Signed-off-by: Héctor De Diego <lecksfrawen@gmail.com>

---
+++ CheckValues.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var originalPitch: Double = 0.0
+var pitchValues: [Double] = []
+
+// Obtener el rango minimo permitido
+// Revisar que el valor actual no se salga del rango permitodo
+// Si se sale, mostrar en el print que se haya salido o no del rango permitido
+
+//let currentPitchIsHigher = pitchValues > originalPitch
+//let currentPitchIsLower = pitchValues < originalPitch
+//let hasMoved = currentPitchIsHigher || currentPitchIsLower
+
+print("Has moved: \(hasMoved ? "YES" : "NO")")
