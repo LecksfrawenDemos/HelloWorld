f3ef29ea91d376d2366edcf2d22b2a6367326522
Héctor De Diego <lecksfrawen@gmail.com>
2019-02-20 13:07:49 -0600

    Revisar valores
    Signed-off-by: Héctor De Diego <lecksfrawen@gmail.com>

--- CheckValues.playground/Contents.swift
+++ CheckValues.playground/Contents.swift
@@ -1,7 +1,59 @@
 import UIKit
 
-var originalPitch: Double = 0.0
-var pitchValues: [Double] = []
+let minRange: Double = 0.1
+let maxRange: Double = 0.40
+var originalPitchRange: ClosedRange = minRange...maxRange
+var pitchValues: [Double] = [
+  0.0045,
+  0.0044,
+  0.0047,
+  0.0043,
+  1.5169,
+  0.7984,
+  0.0074,
+  0.0068,
+  0.0069,
+  0.0059,
+  0.0057,
+  0.0054,
+  0.0050,
+  0.0051,
+  0.0045,
+  0.0050,
+  0.0046,
+  0.0047,
+  0.0050,
+  0.0049,
+  0.0048,
+  0.0048,
+  0.0047,
+  0.0047,
+  0.0047,
+  0.0047,
+  0.0047,
+  0.0046,
+  0.0048,
+  0.0049,
+  0.0048,
+  0.0050,
+  0.0048,
+  0.0048,
+  0.0050,
+  0.0050,
+  0.0050,
+  0.0050,
+  0.0050,
+  0.0050,
+  0.0050,
+  0.0047,
+  0.0048
+]
+var currentPitchValue: Double = 0.3
+var hasMoved: Bool = false
+switch currentPitchValue {
+  case originalPitchRange: hasMoved = false
+  default: hasMoved = true
+}
 
 // Obtener el rango minimo permitido
 // Revisar que el valor actual no se salga del rango permitodo
@@ -9,6 +61,6 @@ var pitchValues: [Double] = []
 
 //let currentPitchIsHigher = pitchValues > originalPitch
 //let currentPitchIsLower = pitchValues < originalPitch
-//let hasMoved = currentPitchIsHigher || currentPitchIsLower
+//hasMoved = currentPitchIsHigher || currentPitchIsLower
 
 print("Has moved: \(hasMoved ? "YES" : "NO")")
